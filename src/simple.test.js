43084747d893502c5ae62b967f49f7dd29464fdd
Andreas Markussen <andreas@markussen.dk>
2020-03-02 10:14:32 +0100

    Added a few tests

---
+++ src/simple.test.js
@@ -0,0 +1,23 @@
+import { simpleFn } from './simple';
+
+test("SimpleTest Suite1", () => {
+  const simpleAnswer = simpleFn(1);
+  expect(simpleAnswer).toBe(1);
+});
+
+test("SimpleTest Suite2", () => {
+  const simpleAnswer = simpleFn(1);
+  expect(simpleAnswer).toBe(1);
+});
+
+
+test("SimpleTest Suite3", () => {
+  const simpleAnswer = simpleFn(1);
+  expect(simpleAnswer).toBe(1);
+});
+
+
+test("SimpleTest Suite4", () => {
+  const simpleAnswer = simpleFn(1);
+  expect(simpleAnswer).toBe(1);
+});
